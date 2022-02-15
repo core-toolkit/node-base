1db5bc3fe7efe8560bb8c88f33669f7df0366fbe
tecs <ttecss@gmail.com>
2022-02-15 19:02:58 +0200

    Fixed issues with the templates

--- src/cli/templates/src/application/UseCase.test.js
+++ src/cli/templates/src/application/UseCase.test.js
@@ -4,7 +4,7 @@ describe('__name__UseCase', () => {
   describe('.getAll()', () => {
     it('fetches a list of all __name__ entries', async () => {
       const mock = jest.fn();
-      const useCase = __name__UseCase({ Models: { __name__: { findAll: mock } } });
+      const useCase = __name__UseCase({ Model: { __name__: { findAll: mock } } });
 
       const items = await useCase.getAll();
       expect(items).toContain(expect.objectContaining({ id: 1, name: 'foo', value: 'abc' }));
@@ -15,7 +15,7 @@ describe('__name__UseCase', () => {
   describe('.getById()', () => {
     it('fetches a single __nameLower__', async () => {
       const mock = jest.fn();
-      const useCase = __name__UseCase({ Models: { __name__: { findByPk: mock } } });
+      const useCase = __name__UseCase({ Model: { __name__: { findByPk: mock } } });
 
       const item = await useCase.getById(1);
       expect(item).toBe(expect.objectContaining({ id: 1, name: 'foo', value: 'abc', extendedProperty: 'extra' }));
@@ -23,7 +23,7 @@ describe('__name__UseCase', () => {
 
     it('does not fetch missing __nameLower__ items', async () => {
       const mock = jest.fn();
-      const useCase = __name__UseCase({ Models: { __name__: { findByPk: mock } } });
+      const useCase = __name__UseCase({ Model: { __name__: { findByPk: mock } } });
 
       const item = await useCase.getById(-1);
       expect(item).toBeFalsy();
@@ -33,7 +33,7 @@ describe('__name__UseCase', () => {
   describe('.create()', () => {
     it('creates a __nameLower__', async () => {
       const mock = jest.fn();
-      const useCase = __name__UseCase({ Models: { __name__: { create: mock } } });
+      const useCase = __name__UseCase({ Model: { __name__: { create: mock } } });
 
       const newItem = await useCase.create('test name', 'test value');
       expect(typeof newItem.id).toBe('number');
