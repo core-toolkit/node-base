97ac6678e39bc27cc1ee669311ffa7987d24b2d2
tecs <ttecss@gmail.com>
2022-02-19 01:20:15 +0200

    Expanded the Obj util to handle built-ins during deepCopy() and added deepEquals()

--- src/utils/Obj.js
+++ src/utils/Obj.js
@@ -8,17 +8,71 @@ const bindMethods = (obj) => {
     .forEach(method => (obj[method] = obj[method].bind(obj)));
 };
 
+const primitivable = [Boolean, Number];
+const stringable = [String, Date, RegExp];
+const arrayable = [Int8Array, Uint8Array, Uint8ClampedArray, Int16Array, Uint16Array, Int32Array, Uint32Array, Float32Array, Float64Array, BigInt64Array, BigUint64Array, Map, Set]
+const safelyConstructable = primitivable.concat(stringable, arrayable);
+
+const is = (obj, constructors) => constructors.some((Type) => obj instanceof Type);
+const reconstruct = (obj) => new obj.constructor(obj);
+const toPrimitive = (obj) => obj.constructor(obj);
 /**
- * @param {Object<T>}
+ * @param {Object<T>} obj
+ * @param {Boolean} unsafe
  * @returns {Object<T>}
  */
-const deepCopy = (obj) => {
-  if (Array.isArray(obj)) return obj.map(deepCopy);
-  if (obj && typeof obj === 'object') return Object.keys(obj).reduce((o, k) => (o[k] = deepCopy(obj[k]), o), {});
-  return obj;
+const deepCopy = (obj, unsafe = false) => {
+  if (Array.isArray(obj)) return obj.map((e) => deepCopy(e, unsafe));
+  if (typeof obj !== 'object' || !obj) return obj;
+
+  if (obj instanceof Buffer) return Buffer.from(obj);
+  const safe = is(obj, safelyConstructable);
+  if ((safe || unsafe) && Object.getPrototypeOf(obj) !== Object.prototype) return reconstruct(obj);
+
+  const newObj = {};
+  for (const key of Object.keys(obj)) {
+    newObj[key] = deepCopy(obj[key], unsafe)
+  }
+  return newObj;
+};
+
+const deepEquals = (obj1, obj2) => {
+  if (is(obj1, primitivable)) {
+    obj1 = toPrimitive(obj1);
+  } else if (is(obj1, stringable)) {
+    obj1 = String(obj1);
+  }
+
+  if (is(obj2, primitivable)) {
+    obj2 = toPrimitive(obj2);
+  } else if (is(obj2, stringable)) {
+    obj2 = String(obj2);
+  }
+
+  if (obj1 === obj2) return true;
+
+  const type = typeof obj1;
+  if (type !== 'object' || type !== typeof obj2 || obj1 === null || obj2 === null) return false;
+  if (obj1.constructor !== obj2.constructor) return false;
+
+  const isArr = Array.isArray(obj1);
+  if (isArr) return obj1.length === obj2.length && obj1.every((el, i) => deepEquals(el, obj2[i]));
+
+
+  if (is(obj1, arrayable)) {
+    return deepEquals([...obj1], [...obj2]);
+  }
+
+  if ([obj1, obj2].some((obj) => Object.getPrototypeOf(obj) !== Object.prototype)) return false;
+
+  const keys = Object.keys(obj1);
+  if (!deepEquals(keys, Object.keys(obj2))) return false;
+
+  return keys.every((key) => deepEquals(obj1[key], obj2[key]));
 };
 
 module.exports = {
   bindMethods,
   deepCopy,
+  deepEquals,
 };
--- src/utils/Obj.test.js
+++ src/utils/Obj.test.js
@@ -32,35 +32,136 @@ describe('Obj', () => {
   });
 
   describe('.deepCopy()', () => {
-    it('copies objects', () => {
-      const obj = {
-        foo: [123, 456],
-        bar: { x: 'abc' },
-        baz: true,
-      };
-
-      const copy = Obj.deepCopy(obj);
-      expect(copy).toHaveProperty('foo', expect.arrayContaining([123, 456]));
-      expect(copy).toHaveProperty('bar', expect.objectContaining({ x: 'abc' }));
-      expect(copy).toHaveProperty('baz', true);
+    it('copies primitives', () => {
+      expect(Obj.deepCopy('foo')).toBe('foo');
+      expect(Obj.deepCopy(5)).toBe(5);
+      expect(Obj.deepCopy(null)).toBe(null);
+      expect(Obj.deepCopy(undefined)).toBe(undefined);
+      expect(Obj.deepCopy(false)).toBe(false);
     });
 
-    it('derefences copied objects', () => {
-      const obj = {
-        foo: [123, 456],
-        bar: { x: 'abc' },
-        baz: true,
-      };
+    it('copies arrays', () => {
+      const src = [1, 2, 3];
+      const copy = Obj.deepCopy(src);
+      expect(copy).toEqual(src);
+      expect(copy).not.toBe(src);
+    });
 
-      const copy = Obj.deepCopy(obj);
+    it('copies objects', () => {
+      const src = { foo: 'bar', baz: 'qux' };
+      const copy = Obj.deepCopy(src);
+      expect(copy).toEqual(src);
+      expect(copy).not.toBe(src);
+    });
 
-      copy.foo.pop();
-      delete copy.bar.x;
-      copy.baz = false;
+    it('copies deeply-nested structures', () => {
+      const src = { foo: { bar: [ { baz: 'test' }] }, qux: [1, 2, 3] };
+      const copy = Obj.deepCopy(src);
+      expect(copy).toEqual(src);
+      expect(copy).not.toBe(src);
 
-      expect(obj).toHaveProperty('foo', expect.arrayContaining([123, 456]));
-      expect(obj).toHaveProperty('bar', expect.objectContaining({ x: 'abc' }));
-      expect(obj).toHaveProperty('baz', true);
+      copy.foo.bar[0].baz = 'changed';
+      expect(src.foo.bar[0].baz).toBe('test');
+    });
+
+    it('copies safe instances', () => {
+      const Foo = class { constructor(bar) { this.bar = bar; } };
+      const src = [ new Date(), new Set(['a', 'b', 'c']), Buffer.from([1, 2, 3]), new Foo('qux') ];
+      const copy = Obj.deepCopy(src);
+      expect(copy).toEqual(src);
+      expect(copy).not.toBe(src);
+
+      expect(copy[3]).not.toBeInstanceOf(Foo);
+      expect(copy[3].bar).toBe('qux');
+
+      copy[3].bar = 'quux';
+      expect(src[3].bar).toBe('qux');
+    });
+
+    it('copies unsafe instances', () => {
+      const Foo = class { constructor(bar) { this.bar = bar?.bar ?? bar; } };
+
+      const src = { baz: new Foo('qux') };
+      const copy = Obj.deepCopy(src, true);
+      expect(copy).toEqual(src);
+      expect(copy).not.toBe(src);
+
+      expect(copy.baz).toBeInstanceOf(Foo);
+      expect(copy.baz.bar).toBe('qux');
+
+      copy.baz.bar = 'quux';
+      expect(src.baz.bar).toBe('qux');
+    });
+  });
+
+  describe('.deepEquals()', () => {
+    it('compares primitives', () => {
+      expect(Obj.deepEquals('foo', 'foo')).toBe(true);
+      expect(Obj.deepEquals('foo', 'bar')).toBe(false);
+      expect(Obj.deepEquals(5, 5)).toBe(true);
+      expect(Obj.deepEquals(5, 6)).toBe(false);
+      expect(Obj.deepEquals(null, null)).toBe(true);
+      expect(Obj.deepEquals(undefined, undefined)).toBe(true);
+      expect(Obj.deepEquals(false, false)).toBe(true);
+      expect(Obj.deepEquals(true, false)).toBe(false);
+    });
+
+    it('compares arrays', () => {
+      const src = [1, 2, 3];
+      expect(Obj.deepEquals([], [])).toBe(true);
+      expect(Obj.deepEquals(src, [1, 2, 3])).toBe(true);
+      expect(Obj.deepEquals(src, [1, 2, 3, 4])).toBe(false);
+      expect(Obj.deepEquals(src, [1, 2])).toBe(false);
+      expect(Obj.deepEquals(src, [1, 3, 2])).toBe(false);
+    });
+
+    it('compares objects', () => {
+      const Foo = class { bar = 'baz'; };
+      const src = { foo: 'bar', baz: 'qux' };
+
+      expect(Obj.deepEquals({}, {})).toBe(true);
+      expect(Obj.deepEquals(src, { foo: 'bar', baz: 'qux' })).toBe(true);
+      expect(Obj.deepEquals(src, { foo: 'bar', baz: 'qux', quux: 123 })).toBe(false);
+      expect(Obj.deepEquals(src, { foo: 'bar' })).toBe(false);
+      expect(Obj.deepEquals(new Foo(), new Foo())).toBe(false);
+    });
+
+    it('compares deeply-nested structures', () => {
+      const src = { foo: { bar: [ { baz: 'test' }] }, qux: [1, 2, 3] };
+      const equals = Obj.deepEquals(src, { foo: { bar: [ { baz: 'test' }] }, qux: [1, 2, 3] });
+      expect(equals).toBe(true);
+    });
+
+    it('compares references', () => {
+      const src = [ new Date(), new Set(['foo']), Buffer.from([1, 2, 3]), { foo: 'bar' } ];
+      expect(Obj.deepEquals(src, src)).toBe(true);
+      expect(Obj.deepEquals(src[0], src[0])).toBe(true);
+      expect(Obj.deepEquals(src[1], src[1])).toBe(true);
+      expect(Obj.deepEquals(src[2], src[2])).toBe(true);
+      expect(Obj.deepEquals(src[3], src[3])).toBe(true);
+    });
+
+    it('compares instances of built-ins', () => {
+      expect(Obj.deepEquals(new Int8Array([1, 2]), new Int8Array([1, 2]))).toBe(true);
+      expect(Obj.deepEquals(new Int8Array([1, 2]), new Int8Array([1, 2, 3]))).toBe(false);
+      expect(Obj.deepEquals(new Set([1, 2]), new Set([1, 2]))).toBe(true);
+      expect(Obj.deepEquals(new Set([1, 2]), new Set([1, 2, 2]))).toBe(true);
+      expect(Obj.deepEquals(new Set([1, 2]), new Set([1, 2, 3]))).toBe(false);
+      expect(Obj.deepEquals(new Map([[1, 2], [2, 3]]), new Map([[1, 2], [2, 3]]))).toBe(true);
+      expect(Obj.deepEquals(new Map([[1, 2], [2, 3]]), new Map([[1, 2], [2, 3], [3, 4]]))).toBe(false);
+      expect(Obj.deepEquals(new RegExp(/bla/g), /bla/g)).toBe(true);
+      expect(Obj.deepEquals(new String('asd'), new String('asd'))).toBe(true);
+      expect(Obj.deepEquals('qwe', new String('qwe'))).toBe(true);
+      expect(Obj.deepEquals(new Number(5), 5)).toBe(true);
+      expect(Obj.deepEquals(true, new Boolean(5))).toBe(true);
+    });
+
+    it('compares items of different types', () => {
+      expect(Obj.deepEquals(null, undefined)).toBe(false);
+      expect(Obj.deepEquals([], {})).toBe(false);
+      expect(Obj.deepEquals({}, null)).toBe(false);
+      expect(Obj.deepEquals(new Date(), new Set())).toBe(false);
+      expect(Obj.deepEquals(new Int8Array([1, 2]), new Int16Array([1, 2]))).toBe(false);
     });
   });
 });
