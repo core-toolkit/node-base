eb460bcd6651debfe26df1eae55e70fe4e7e05dc
tecs <ttecss@gmail.com>
2022-02-18 22:59:18 +0200

    Expanded the jest ignore paths

--- src/cli/templates/jest.config.js
+++ src/cli/templates/jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.js', '!src/*.js', '!src/**/(routes|controllers|clients)/*.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/*.js',
+    '!src/application/routes/*.js',
+    '!src/application/controllers/*.js',
+    '!src/infrastructure/clients/*.js',
+    '!src/infrastructure/models/*.js',
+    '!src/infrastructure/migrations/*.js',
+  ],
   testMatch: ['**/*.test.js'],
   testPathIgnorePatterns: ['./node_modules/', './packages/'],
   silent: false,
